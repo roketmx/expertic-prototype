9588bddc6262b7097765a25e76d46e4e7894dd76
Gustavo Rodriguez <gustavo@roket.mx>
2015-01-27 21:59:14 -0700

    Port mongo

--- api/config.js
+++ api/config.js
@@ -32,9 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 else {
 	module.exports = {
-		name: 'RESTful API - ExperTIC',
+		name: 'RESTful API - Roket',
 		enviroment: 'development',
-		db: 'mongodb://127.0.0.1/expertic-dev',
+		db: 'mongodb://127.0.0.1:27018/expertic-dev',
 		keys: {
 			movil: 'bd90b560496a464c303794163b9cd60b1d301adf17179dd122f994cedeccbe484a0e049acf643c09f3f2f4fcedb04f9c8d3549f95374875e36f531b3874b04b5',
 			app: 'bd90b560496a464c303794163b9cd60b1d301adf17179dd122f994cedeccbe484a0e049acf643c09f3f2f4fcedb04f9c8d3549f95374875e36f531b3874b04b5'
